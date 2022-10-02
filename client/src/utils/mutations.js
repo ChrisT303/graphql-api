42eaa0b9238cfd2eedc5e565778c803c09cfd161
deftonechris <deftonechris@msn.com>
2022-10-02 14:29:56 -0600

    Save_book mutation executing in handleSavedBook() instead of saveBook() import

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,5 @@
 import { gql } from "@apollo/client";
 
-
 export const LOGIN_USER = gql`
 mutation login($email: String!, $password: !String){
     login(email: $email, password: $password) {
@@ -11,54 +10,52 @@ mutation login($email: String!, $password: !String){
         }
     }
 }
-;`
+;`;
 
-export const ADD_USER = gql `
-mutation addUser($username: String!, $email: String!, $password: String!){
-    assUser(username: $username, email: $email, password: $password){
-        token
-        user{
-            _id
-            username 
-            email
-        }
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    assUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
     }
-}
-`
-;
+  }
+`;
 
 export const SAVE_BOOK = gql`
-mutation saveBook($input: SavedBookInput){
-    saveBook(input: $input){
-        username
-        _id
-        bookCount
-        savedBooks {
-           bookId 
-           authors 
-           title 
-           image 
-           link 
-           description 
-        }
+  mutation saveBook($input: SavedBookInput) {
+    saveBook(input: $input) {
+      username
+      _id
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        image
+        link
+        description
+      }
     }
-}
-`
+  }
+`;
 export const REMOVE_BOOK = gql`
-mutation removeBook($bookId: String!) {
+  mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
-        _id
-        username
-        bookCount
-        savedBooks {
-            bookId 
-            authors
-            title 
-            image
-            link 
-            description 
-        }
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        image
+        link
+        description
+      }
     }
-}
-`
-;
+  }
+`;
